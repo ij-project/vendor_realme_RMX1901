d2d307c45929890d3d2a0029113f46465a17bbca
Ij Israfil <israfilkhan494@gmail.com>
2026-06-07 17:31:02 +0000

    RMX1091: Include sensor multihal service
    Fixes:
    FAILED: ninja: out/soong/build.lineage_RMX1901.3.ninja:1138665: multiple rules generate out/target/product/RMX1901/symbols/vendor/bin/hw/android.hardware.sensors@2.0-service.multihal
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- RMX1901-vendor.mk
+++ RMX1901-vendor.mk
@@ -110,6 +110,7 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/android.hardware.health@2.1-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.health@2.1-service.rc \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@4.0-service-qti.rc \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc \
+    vendor/realme/RMX1901/proprietary/vendor/etc/init/android.hardware.sensors@2.0-service-multihal.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@2.0-service-multihal.rc \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/cnd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/cnd.rc \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/com.qualcomm.qti.wifidisplayhal@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/com.qualcomm.qti.wifidisplayhal@1.0-service.rc \
     vendor/realme/RMX1901/proprietary/vendor/etc/init/dataadpl.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dataadpl.rc \
@@ -897,6 +898,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsuceaidlservice-V1-java \
     vendor.qti.latency-V2.0-java \
     android.hardware.health@2.1.xml \
+    android.hardware.sensors@2.0-multihal.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
     ATFWD-daemon \
     adpl \
@@ -915,6 +917,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service-qti \
     android.hardware.keymaster@4.0-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti \
+    android.hardware.sensors@2.0-service.multihal \
     qcrild \
     vendor.display.color@1.0-service \
     vendor.oppo.hardware.biometrics.fingerprint@2.1-service \
